a8e8046f3b9c030d27e013f4d7e33313d12e5832
Cesar <guto_montaldi@yahoo.com.br>
2024-01-17 13:45:55 -0300

    Configurando as propriedades de conexão.

--- src/test/java/cesarMontaldi/javaMail/AppTest.java
+++ src/test/java/cesarMontaldi/javaMail/AppTest.java
@@ -1,20 +1,21 @@
 package cesarMontaldi.javaMail;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Properties;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+	
+	@Test
+	public void testEmail() {
+		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
+		properties.put("mail.smtp.port", "465");/* Porta do servidor */
+		properties.put("mail.smtp.socketDactory.port", "465");/* Especifica a porta a ser conectada pelo Socket */
+		properties.put("mail.smtp.socketDactory.class", "javax.net.ssl.SSLSocketFactory");/* Clase socket de conexão ao SMTP */
+	}
 }
