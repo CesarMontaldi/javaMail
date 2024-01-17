b7fca381b76e0c74128bacb9f69f15d95c22f13a
Cesar <guto_montaldi@yahoo.com.br>
2024-01-17 16:20:00 -0300

    Criado a classe EmailSend, realizando testes com Junit.

---
+++ src/main/java/cesarMontaldi/javaMail/EmailSend.java
@@ -0,0 +1,71 @@
+package cesarMontaldi.javaMail;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSend {
+
+	private String userName = "";
+	private String password = "";
+	
+	private String listaDestinatarios = "";
+	private String nomeRemetente = "";
+	private String assuntoEmail = "";
+	private String textoEmail = "";
+	
+
+	public EmailSend(String listaDestinatarios, String nomeRemetente, String assuntoEmail, String textoEmail) {
+		this.listaDestinatarios = listaDestinatarios;
+		this.nomeRemetente = nomeRemetente;
+		this.assuntoEmail = assuntoEmail;
+		this.textoEmail = textoEmail;
+	}
+
+
+	public void EnviarEmail() {
+
+		try {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.ssl.trust", "*");/* Autenticação SSL */
+			properties.put("mail.smtp.auth", "true");/* Autorização */
+			properties.put("mail.smtp.starttls", "true");/* Autenticação */
+			properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
+			properties.put("mail.smtp.port", "465");/* Porta do servidor */
+			properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser conectada pelo Socket */
+			properties.put("mail.smtp.socketFactory.class",
+					"javax.net.ssl.SSLSocketFactory");/* Clase socket de conexão ao SMTP */
+
+			Session session = Session.getInstance(properties, new Authenticator() {
+
+				protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				};
+
+			});
+
+			Address[] toUser = InternetAddress.parse(listaDestinatarios);
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName, nomeRemetente));/* Quem está enviando */
+			message.setRecipients(Message.RecipientType.TO, toUser);/* Email destinatario */
+			message.setSubject(assuntoEmail);/* Assunto do e-mail */
+			message.setText(textoEmail);
+
+			Transport.send(message);
+
+			System.out.println("E-mail enviado!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/test/java/cesarMontaldi/javaMail/AppTest.java
+++ src/test/java/cesarMontaldi/javaMail/AppTest.java
@@ -1,61 +1,18 @@
 package cesarMontaldi.javaMail;
 
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.junit.Test;
-
 
 public class AppTest {
 	
-	private String userName = "";
-	private String password = "";
-	
 	@Test
-	public void testEmail() {
+	public void emailTest() {
 		
-			try {
-				Properties properties = new Properties();
-				properties.put("mail.smtp.ssl.trust", "*");/* Autenticação SSL */
-				properties.put("mail.smtp.auth", "true");/* Autorização */
-				properties.put("mail.smtp.starttls", "true");/* Autenticação */
-				properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
-				properties.put("mail.smtp.port", "465");/* Porta do servidor */
-				properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser conectada pelo Socket */
-				properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/* Clase socket de conexão ao SMTP */
-				
-				Session session = Session.getInstance(properties, new Authenticator() {
-					
-					protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(userName, password);
-					};
+		EmailSend emailSend = new EmailSend("guto_montaldi@yahoo.com.br",
+											"Cesar Montaldi",
+											"Testanto e-mail com Java",
+											"Olá parábens você está indo super bem, continue assim vai longe.");
+		emailSend.EnviarEmail();
+	} 
 	
-				});
-				
-				Address[] toUser = InternetAddress.parse("guto_montaldi@yahoo.com.br, montaldi35@gmail.com");
-				
-				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(userName, "Cesar Montaldi"));/*Quem está enviando*/
-				message.setRecipients(Message.RecipientType.TO, toUser);/*Email destinatario*/
-				message.setSubject("Chegou um e-mail enviado com Java");/*Assunto do e-mail*/
-				message.setText("Olá programador, você acaba de receber um e-mail enviado com Java do curso Formação Java Web do Jdev Treinamentos.");
-				
-				Transport.send(message);
-				
-				System.out.println("E-mail enviado!");
-				
-			}catch (Exception e) {
-				e.printStackTrace();
-		}
-	}
-
 }
 
