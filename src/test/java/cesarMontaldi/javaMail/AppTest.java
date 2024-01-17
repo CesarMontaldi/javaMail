6de419898ada22927fd5c862569ab469d569846c
Cesar <guto_montaldi@yahoo.com.br>
2024-01-17 14:26:51 -0300

    Configurando conexão com servidor Gmail.

--- src/test/java/cesarMontaldi/javaMail/AppTest.java
+++ src/test/java/cesarMontaldi/javaMail/AppTest.java
@@ -2,20 +2,44 @@ package cesarMontaldi.javaMail;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.Test;
 
 
 public class AppTest {
 	
+	private String userName = "";
+	private String password = "";
+	
 	@Test
 	public void testEmail() {
 		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");/* Autorização */
-		properties.put("mail.smtp.starttls", "true");/* Autenticação */
-		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
-		properties.put("mail.smtp.port", "465");/* Porta do servidor */
-		properties.put("mail.smtp.socketDactory.port", "465");/* Especifica a porta a ser conectada pelo Socket */
-		properties.put("mail.smtp.socketDactory.class", "javax.net.ssl.SSLSocketFactory");/* Clase socket de conexão ao SMTP */
+			try {
+				Properties properties = new Properties();
+				properties.put("mail.smtp.auth", "true");/* Autorização */
+				properties.put("mail.smtp.starttls", "true");/* Autenticação */
+				properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
+				properties.put("mail.smtp.port", "465");/* Porta do servidor */
+				properties.put("mail.smtp.socketDactory.port", "465");/* Especifica a porta a ser conectada pelo Socket */
+				properties.put("mail.smtp.socketDactory.class", "javax.net.ssl.SSLSocketFactory");/* Clase socket de conexão ao SMTP */
+				
+				Session session = Session.getInstance(properties, new Authenticator() {
+					
+					protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(userName, password);
+					};
+	
+				});
+				
+				System.out.println(session);
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+		}
 	}
+
 }
+
